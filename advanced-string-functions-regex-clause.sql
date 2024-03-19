c8731e370df19fc11c0f8b5907633e5c33de6c42
kyle c <163612025+kc4stillo@users.noreply.github.com>
2024-03-18 23:27:06 -0500

    create advanced-string-functions-regex-clause.sql

---
+++ advanced-string-functions-regex-clause.sql
@@ -0,0 +1,53 @@
+-- #1667 fix names in a table
+
+-- #1527 patients with a condition
+SELECT
+    patient_id,
+    patient_name,
+    conditions
+FROM
+    patients
+WHERE
+    conditions LIKE 'DIAB1%'
+    OR conditions LIKE '% DIAB1%'
+  
+-- #196 delete duplicate emails
+DELETE FROM person
+WHERE
+    id NOT IN (
+        SELECT
+            MIN(id)
+        FROM
+            person
+        GROUP BY
+            email
+    )
+  
+-- #176 second highest salary
+SELECT
+    MAX(salary) AS secondhighestsalary
+FROM
+    employee
+WHERE
+    salary NOT IN (
+        SELECT
+            MAX(salary)
+        FROM
+            employee
+    )
+  
+-- #1484 group sold products by the date
+SELECT
+    product_name,
+    SUM(unit) AS unit
+FROM
+    products
+    INNER JOIN orders USING (product_id)
+WHERE
+    order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY
+    product_name
+HAVING
+    SUM(unit) >= 100
+
+-- 1517 find users with valid e-mails
