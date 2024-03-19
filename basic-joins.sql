b88e71af52c1d77b54ae27b4bc22857ddf847702
kyle c <163612025+kc4stillo@users.noreply.github.com>
2024-03-18 23:28:41 -0500

    update basic-joins.sql

--- basic-joins.sql
+++ basic-joins.sql
@@ -1,46 +1,63 @@
 -- #1378 replace employee id with the unique identifier
-SELECT unique_id, name
-FROM Employees
-LEFT JOIN EmployeeUNI
-USING(id);
+SELECT
+    unique_id,
+    name
+FROM
+    employees
+    LEFT JOIN employeeuni USING (id);
 
 -- # 1068 product sales analysis i
-SELECT product_name, year, price
-FROM Sales
-inner JOIN Product
-USING(Product_id)
-
+SELECT
+    product_name,
+    year,
+    price
+FROM
+    sales
+    INNER JOIN product USING (product_id)
+  
 -- $1581 customer who visited but did not make any transactions
-SELECT 
-  customer_id, 
-  COUNT(*) AS count_no_trans 
-FROM 
-  Visits AS v 
-  LEFT JOIN Transactions AS t USING (visit_id) 
-WHERE 
-  t.visit_id IS NULL 
-GROUP BY 
-  customer_id;
+SELECT
+    customer_id,
+    COUNT(*) AS count_no_trans
+FROM
+    visits AS v
+    LEFT JOIN transactions AS t USING (visit_id)
+WHERE
+    t.visit_id IS NULL
+GROUP BY
+    customer_id;
 
 -- #197 rising temperature
 
 -- #1661 average time of process per machine
 
--- #employee bonus
-SELECT name, bonus
-FROM Employee
-LEFT JOIN Bonus
-USING (empId)
-WHERE bonus < 1000 or bonus is null
-
+-- 577 employee bonus
+SELECT
+    name,
+    bonus
+FROM
+    employee
+    LEFT JOIN bonus USING (empid)
+WHERE
+    bonus < 1000
+    OR bonus IS NULL
+  
 -- #1280 students and examinations
-
+  
 -- #570 managers with at least 5 direct reports
-SELECT name
-from employee
-where id in (select managerId
-from employee
-group by managerId
-having count(*) >= 5)
-
--- #1934 confirmation rate
+SELECT
+    name
+FROM
+    employee
+WHERE
+    id IN (
+        SELECT
+            managerid
+        FROM
+            employee
+        GROUP BY
+            managerid
+        HAVING
+            COUNT(*) >= 5
+    )
+    -- #1934 confirmation rate
